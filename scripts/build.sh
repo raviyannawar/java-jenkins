03cb3dbcca12a16b5f13587a41b8087986ccd3b0
raviyannawar <ravi.yannawar@gmail.com>
2024-11-03 23:09:27 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,4 @@
 pwd;
-export maven.wagon.http.ssl.insecure=true;
-export maven.wagon.http.ssl.allowall=true;
 cd firstjava;
 mvn clean install;
 mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true deploy;
