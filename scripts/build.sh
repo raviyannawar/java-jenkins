0a43f1a4657c669b4dffa6a0b3de77938249641d
raviyannawar <ravi.yannawar@gmail.com>
2024-11-03 23:00:34 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 pwd;
+export MAVEN_OPTS="-Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true Dmaven.wagon.http.ssl.ignore.validity.dates=true";
 cd firstjava;
 mvn clean install;
-mvn deploy -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true;
+mvn deploy;
 
