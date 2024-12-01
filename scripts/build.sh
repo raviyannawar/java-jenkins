9eb9c2d10db463e31100c15ab982dda4fd54763b
raviyannawar <ravi.yannawar@gmail.com>
2024-12-01 18:48:59 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,6 @@ pwd;
 cd firstjava;
 mvn clean install;
 export MAVEN_OPTS="-Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.insecure=true"
-mvn  -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true deploy;
+mvn  deploy -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -DrepositoryId=gitlab-maven -DaltDeploymentRepository=gitlab-maven::default::https://${GITLAB_USER}:${GITLAB_TOKEN}@gitlab.internal.local/java-group/java-repositories/packages/maven
+
 
